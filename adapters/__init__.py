08716796a2ffa64a7bc8785daf5c9ccface216dd
amoufaq5 <137143672+amoufaq5@users.noreply.github.com>
2025-08-18 16:34:58 +0300

    Create __init__.py

---
+++ adapters/__init__.py
@@ -0,0 +1,7 @@
+from .referral_adapter import generate_referral
+from .clarification_adapter import clarify
+from .asmethod_adapter import parse_asmethod
+from .diagnosis_adapter import run_diagnosis
+from .fusion_adapter import fuse_results
+from .symptom_classifier_adapter import classify_symptoms
+from .visual_diagnosis_adapter import analyze_image
